2b2865958415effa3191417b6291c05615b4e42b
GonzaloTT <maganagonzalosegovia@gmail.com>
2026-04-19 00:18:41 -0600

    Se agrego AppRouter para navegacion

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Services from "../pages/Services/Services";
+import About from "../pages/About/About";
+import Gallery from "../pages/Gallery/Gallery";
+import Contact from "../pages/Contact/Contact";
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/servicios" element={<Services />} />
+        <Route path="/nosotros" element={<About />} />
+        <Route path="/galeria" element={<Gallery />} />
+        <Route path="/contacto" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
