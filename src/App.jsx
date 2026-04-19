12d6a8dc536b1f29dd633117d5acd6e4631ad0fa
GonzaloTT <maganagonzalosegovia@gmail.com>
2026-04-19 00:24:51 -0600

    Se dejo App.jsx limpio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import { useState } from 'react'
+import AppRouter from "./routes/AppRouter";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 function App() {
-
+  return (
+    <>
+      <Header />
+      <AppRouter />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
